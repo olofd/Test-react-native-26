a21b6043f4beb78f31b7a9bf01168467e1ae7086
Olof Dahlbom <olof.dahlbom@me.com>
2016-05-19 16:51:48 +0200

    Repo

---
+++ ios/TesTest/Test.m
@@ -0,0 +1,23 @@
+//
+//  RCTDNWebHeaderViewManager.m
+//  Prio
+//
+//  Created by Olof Dahlbom (DN) on 2016-04-06.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import "Test.h"
+@implementation Test
+
+
+RCT_EXPORT_MODULE()
+
+- (UIView *)view
+{
+  UIView *headerView = [UIView new];
+  return headerView;
+}
+
+
+
+@end
